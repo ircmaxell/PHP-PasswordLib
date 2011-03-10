972e116079ee283179b9ddfe4540ce18dedc5621
Anthony Ferrara <ircmaxell@gmail.com>
2011-03-10 09:39:14 -0500

    Add a core bootloader to the package

---
+++ lib/CryptLib/bootstrap.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Bootstrap the library.  This registers a simple autoloader for autoloading
+ * classes 
+ *
+ * If you are using this library inside of another that uses a similar 
+ * autoloading system, you can use that autoloader instead of this file.
+ *
+ * PHP version 5.3
+ *
+ * @category   PHPCryptLib
+ * @package    Core
+ * @author     Anthony Ferrara <ircmaxell@ircmaxell.com>
+ * @copyright  2011 The Authors
+ * @license    http://opensource.org/licenses/bsd-license.php New BSD License
+ * @license    http://www.gnu.org/licenses/lgpl-2.1.html LGPL v 2.1
+ */
+
+namespace CryptLib;
+
+/**
+ * The simple autoloader for the CryptLib library.
+ *
+ * @param string $class The class name to load
+ *
+ * @return void
+ */
+$autoload = function ($class) {
+        if (substr($class, 0, 8) != 'CryptLib') return;
+        $path = substr(str_replace('\\', '/', $class), 8);
+        $path = __DIR__ . $path . '.php';
+        if (file_exists($path)) {
+                require_once $path;
+        }
+};
+
+//Register the autoloader with spl
+\spl_autoload_register($autoload);
