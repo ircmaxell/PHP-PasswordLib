5a89a147bcdd6ca6e16053ff3c2c311890366d13
Anthony Ferrara <ircmaxell@gmail.com>
2011-03-15 07:16:36 -0400

    Add another test case

--- test/Unit/Core/BaseConverterTest.php
+++ test/Unit/Core/BaseConverterTest.php
@@ -5,7 +5,7 @@ use CryptLib\Core\BaseConverter;
 class Unit_Core_BaseConverterTest extends PHPUnit_Framework_TestCase {
 
     public static function provideConvertFromBinary() {
-        return array(
+        $return = array(
             array('', '', ''),
             array(chr(0), '012', '0'),
             array(chr(1), '012', '1'),
@@ -16,6 +16,14 @@ class Unit_Core_BaseConverterTest extends PHPUnit_Framework_TestCase {
             array(chr(1) . chr(2) . chr(3), '0123456789', '66051'),
             array(chr(155) . chr(255), '0123456789abcdef', '9bff'),
         );
+        $build = array('', '0123456789abcdef', '');
+        for ($i = 1; $i < 256; $i++) {
+            $build[0] .= chr($i);
+            $build[2] .= str_pad(dechex($i), 2, '0', \STR_PAD_LEFT);
+        }
+        $build[2] = ltrim($build[2], '0');
+        $return[] = $build;
+        return $return;
     }
 
     /**
