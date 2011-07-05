4d763496bdc4c46a049c9afe81bdb0a9885d2a30
Anthony Ferrara <ircmaxell@gmail.com>
2011-07-05 13:33:29 -0400

    Add OpenSSL random number source

---
+++ test/Unit/Random/Source/OpenSSLTest.php
@@ -0,0 +1,41 @@
+<?php
+
+use CryptLib\Random\Source\OpenSSL;
+use CryptLib\Core\Strength\High as HighStrength;
+
+class Unit_Random_Source_OpenSSLTest extends PHPUnit_Framework_TestCase {
+
+    public static function provideGenerate() {
+        $data = array();
+        for ($i = 0; $i < 100; $i += 5) {
+            $not = $i > 0 ? str_repeat(chr(0), $i) : chr(0);
+            $data[] = array($i, $not);
+        }
+        return $data;
+    }
+
+    /**
+     * @covers CryptLib\Random\Source\MCrypt::getStrength
+     */
+    public function testGetStrength() {
+        $strength = new HighStrength;
+        $actual = OpenSSL::getStrength();
+        $this->assertEquals($actual, $strength);
+    }
+
+    /**
+     * @covers CryptLib\Random\Source\MCrypt::generate
+     * @dataProvider provideGenerate
+     */
+    public function testGenerate($length, $not) {
+        $rand = new OpenSSL;
+        $stub = $rand->generate($length);
+        $this->assertEquals($length, strlen($stub));
+        if (function_exists('openssl_random_pseudo_bytes')) {
+            $this->assertNotEquals($not, $stub);
+        } else {
+            $this->assertEquals(str_repeat(chr(0), $length), $stub);
+        }
+    }
+
+}
